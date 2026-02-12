32a1268409ce7a51c587700b6b370e8c63042208
Vishuddhanand <vishupillewan2006@gmail.com>
2026-02-12 23:29:32 +0530

    initialize project

---
+++ src/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        unique:[true , "Username already exists"],
+        required:[true , "Username is required"]
+    },
+    email:{
+        type:String,
+        unique:[true , "Email already exists"],
+        required:[true , "Email is required"]
+    },
+    password:{
+        type:String,
+        required:[true , "Password is required"]
+    },
+    bio:String,
+    profileImage:{
+        type:String,
+        default:"https://ik.imagekit.io/heu4aa5wj/insta-clone/insta%20default%20img.jpg"
+    }
+
+})
+
+const userModel = mongoose.model("users", userSchema)
+
+module.exports = userModel
---
+++ src/routes/user.routes.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const userModel = require("../models/user.model")
+const authRouter = express.Router()
+const crypto = require("crypto")
+const jwt = require("jsonwebtoken")
+
+
+authRouter.post("/register", async (req, res) => {
+    const { username, email, password, bio, profileImage } = req.body
+
+    const isUserAlreadyExists = await userModel.findOne({
+        $or: [
+            { username },
+            { password }
+        ]
+    })
+
+    if (isUserAlreadyExists) {
+        return res.status(409)
+            .json({
+                messaage: "User Already exists" + (isUserAlreadyExists.username == username ? "Username Already Exists" : "Email Already Exists")
+            })
+    }
+
+    const hash = crypto.createHash("sha256").update(password).digest("hex")
+
+    const user = await userModel.create({
+        username,
+        email,
+        password: hash,
+        bio,
+        profileImage
+    })
+
+    const token = jwt.sign(
+        {
+            id:user._id
+        },
+        process.env.JWT_SECRET,
+        {expiresIn:"1d"}
+
+    )
+
+    res.cookie("token", token )
+
+    res.staatus(201).json({
+        messaage:"user created successfully"
+    })
+
+
+
+})
