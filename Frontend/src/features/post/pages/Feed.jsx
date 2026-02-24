a30a491605cc9b6ae8eb692f17eda841824d68f8
Vishuddhanand <vishupillewan2006@gmail.com>
2026-02-25 00:31:00 +0530

    implement Post component and update Feed layout

--- Frontend/src/features/post/pages/Feed.jsx
+++ Frontend/src/features/post/pages/Feed.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import "../style/feed.scss"
+import Post from '../components/Post'
 
 const Feed = () => {
     return (
         <main className="feed-page">
             <div className="feed">
                 <div className="posts">
-                    <div className="post">
-
-                        <div className="user">
-                            <div className="img-wrapper">
-
-                                <img src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                            </div>
-                            <p>username</p>
-                        </div>
-                        <img src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-                        <div className="bottom">
-                            <div className="caption">sample caption</div>
-                        </div>
-                    </div>
+                    <Post />
                 </div>
 
             </div>
